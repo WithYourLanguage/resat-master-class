8f9b0810d28898137768a2d5939a0ef6764e66ce
강 <ghang0501@gmail.com>
2023-05-29 21:43:47 +0900

    2.7 dark mode & light mode 변경 50%
    2.7강의에서는 dark모드와 light모드를 변경하는 것을 50%완성했다. 이용자가
      직접 코드를 변경하지 않고 변경하는 기능은 추후 배워볼 예정이다

--- src/App.js
+++ src/App.js
@@ -2,52 +2,21 @@ import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 20px;
-  }
-  50% {
-    border-radius: 100px;
-
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 20px;
-  }
-`;
-
-const Emoji = styled.span`
-  color: white;
-`;
-
-const Box = styled.div`
-  height: 100px;
-  width: 100px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 1.5s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 30px;
-    }
-  }
+const Title = styled.span`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
-    <Father>
-      <Box>
-        <Emoji>Happy </Emoji>
-      </Box>
+    <Father as="body">
+      <Title>Happy </Title>
     </Father>
   );
 }
