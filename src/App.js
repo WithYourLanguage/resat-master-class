527ce86ecd5f1bde784699d88950a7aa2d6cb702
강 <ghang0501@gmail.com>
2023-05-29 20:51:35 +0900

    2.4 styled-components로 애니메이션 만들기
    2.4강의에서는 styled-components로 애니메이션 만드는 방법에 대해 배웠음.
      그 이외에도 &을 이용해 :hover과 같은 속성을 이용하는 방법도 같이
      배웠음

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
 `;
-const Input = styled.input.attrs({ required: true })`
+
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 20px;
+  }
+  50% {
+    border-radius: 100px;
+
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 20px;
+  }
+`;
+
+const Box = styled.div`
+  height: 100px;
+  width: 100px;
   background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1.5s linear infinite;
+  span {
+    color: white;
+    &:hover {
+      font-size: 30px;
+    }
+  }
 `;
 
 function App() {
   return (
     <Father>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
+      <Box>
+        <span>Happy </span>
+      </Box>
     </Father>
   );
 }
