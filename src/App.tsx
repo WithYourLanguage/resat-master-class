a81ec91b1b320a347ec7248415020fd5eee98446
강 <ghang0501@gmail.com>
2023-05-30 22:35:59 +0900

    3.5 Forms
    Form의 Event를 감지하는 것과 TS에서 어떻게 event의 타입을 지정해주는지
    배웠다

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import styled from "styled-components";
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello!", value)
+  }
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" text="Hellos" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log In</button>
+      </form>
     </div>
   );
 }
