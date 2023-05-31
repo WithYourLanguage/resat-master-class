2cd62a545a6c3d0cfbda22f23b258a8d234ba46c
강 <ghang0501@gmail.com>
2023-05-31 21:38:07 +0900

    4.2 createBrowserRouter
    이번 강의에서는 createBrowserRouter로 Route를 나누는 방법에 대해
    배웠다

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
-import App from "./App";
-import { lightTheme, darkTheme } from "./theme";
+import { RouterProvider } from "react-router-dom";
+import router from "./screens/Router";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById("root")
 );
