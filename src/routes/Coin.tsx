454346223f38cea8902cd40a34cadeb37da1ded0
강 <ghang0501@gmail.com>
2023-06-02 20:29:39 +0900

    5.7 Nested Routes part one
    강의 5.7에서는 2개의 라우터를 더 만들어 보았다

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,8 @@
-import { useLocation, useParams } from "react-router-dom";
-import styled from "styled-components";
 import { useEffect, useState } from "react";
+import { Switch, Route, useLocation, useParams } from "react-router";
+import styled from "styled-components";
+import Chart from "./Chart";
+import Price from "./Price";
 
 interface RouteParams {
   coinId: string;
@@ -28,10 +30,40 @@ const Title = styled.h1`
   font-size: 48px;
 `;
 
-const Loading = styled.span`
+const Loader = styled.span`
   text-align: center;
   display: block;
 `;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+interface RouteParams {
+  coinId: string;
+}
+interface RouteState {
+  name: string;
+}
 
 interface InfoData {
   // id: string;
@@ -79,7 +111,7 @@ interface PriceData {
     USD: {
       ath_date: string;
       ath_price: number;
-      market_cap: number
+      market_cap: number;
       market_cap_change_24h: number;
       percent_change_1h: number;
       percent_change_1y: number;
@@ -89,7 +121,7 @@ interface PriceData {
       percent_change_15m: number;
       percent_change_24h: number;
       percent_change_30d: number;
-      percent_change_30m: number
+      percent_change_30m: number;
       percent_from_price_ath: number;
       price: number;
       volume_24h: number;
@@ -103,7 +135,7 @@ function Coin() {
   const [loading, setLoading] = useState(true);
   const { state } = useLocation<RouteState>();
   const [info, setInfo] = useState<InfoData>();
-  const [priveInfo, setPriceInfo] = useState<PriceData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
   useEffect(() => {
     (async () => {
       const infoData = await (
@@ -113,18 +145,59 @@ function Coin() {
       const priceData = await (
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
-      
+
       setInfo(infoData);
       setPriceInfo(priceData);
+      setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
 
   return (
     <Container>
       <Header>
-        <Title>{state?.name || "Loading..."}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
       </Header>
-      {loading ? <Loading>Loading...</Loading> : null}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Switch>
+            <Route path={`/${coinId}/price`}>
+              <Price />
+            </Route>
+            <Route path={`/${coinId}/chart`}>
+              <Chart />
+            </Route>
+          </Switch>
+        </>
+      )}
     </Container>
   );
 }
