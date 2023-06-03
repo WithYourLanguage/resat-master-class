2834d3927003d9af143d450f08f2599158b86038
강 <ghang0501@gmail.com>
2023-06-03 12:43:43 +0900

    5.13 Price Chart part Two
    강의 5.13에서는 차트를 만드는 방법에 대해 배웠다

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,15 +1,71 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import Apexchart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
 
 interface CharProps {
   coinId: string;
 }
 
 function Chart({ coinId }: CharProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <Apexchart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close) as unknown as number[],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: {
+              show: false,
+            },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
