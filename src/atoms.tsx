1eac9c344486b98385d1cc8c7a97e816c7ca3b0d
강 <ghang0501@gmail.com>
2023-06-07 16:26:28 +0900

    6.16 Sellectors part One
    강의 6.16에서는 코드를 조금 더 깔끔하게 만드는 작업을 진행하였다

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -4,6 +4,12 @@ export interface IToDo {
   category: "TO_DO" | "DOING" | "DONE";
   id: number;
 }
+
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO",
+});
+
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
 
@@ -14,11 +20,8 @@ export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
     const toDos = get(toDoState);
+    const category = get(categoryState);
 
-    return [
-      toDos.filter((toDo) => toDo.category === "TO_DO"),
-      toDos.filter((toDo) => toDo.category === "DOING"),
-      toDos.filter((toDo) => toDo.category === "DONE"),
-    ];
+    return toDos.filter((toDo) => toDo.category === category);
   },
 });
