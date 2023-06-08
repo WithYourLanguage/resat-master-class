54ae89bec2476c0cce3783e3df9fd06a593c03d5
강 <ghang0501@gmail.com>
2023-06-08 23:59:32 +0900

    8.12 Slider part One
    강의 8.12에서는 슬라이더를 만들어 보았다

--- src/App.tsx
+++ src/App.tsx
@@ -14,108 +14,76 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-  // background: ;
-`;
-
-const BiggerBox = styled.div`
-  width: 500px;
-  height: 500px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  // overflow: hidden;
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
-  width: 100px;
+  width: 400px;
   height: 100px;
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 100px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 15px;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
-`;
+const box = {
+  invisible: {
+    x: 500,
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+  exit: {
+    x: -500,
+    opacity: 0,
+    scale: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
 
 // const boxVariants = {
 //   hover: { scale: 1.5, rotateZ: 90 },
 //   click: { scale: 1, borderRadius: "100px" },
 // };
 
-const Svg = styled.svg`
-  width: 300px;
-  height: 300px;
-  path {
-    stroke: white;
-    stroke-width: 2;
-  }
-`;
-
-const boxVariants = {
-  initial: {
-    opacity: 0,
-    scale: 0,
-  },
-  visible: {
-    opacity: 1,
-    scale: 1,
-    rotateZ: 360,
-  },
-  leaving: {
-    opacity: 0,
-    scale: 0,
-    y: 50,
-  },
-};
-
 function App() {
-  const x = useMotionValue(0);
-  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
-  const gradient = useTransform(
-    x,
-    [-800, 800],
-    [
-      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+  const [visiblle, setVisiblle] = useState(1);
+  const nextPlease = () => setVisiblle((prev) => (prev === 10 ? 10 : prev + 1));
+  const prevPlease = () => setVisiblle((prev) => (prev === 1 ? 1 : prev - 1));
 
-      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 222, 0))",
-    ]
-  );
-  const { scrollYProgress } = useViewportScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-
-  const svg = {
-    start: { pathLength: 0, fill: "rgba(255,255,255,0)" },
-    end: {
-      fill: "rgba(255,255,255,1)",
-      pathLength: 1,
-    },
-  };
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click me</button>
-
       <AnimatePresence>
-        {" "}
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visiblle ? (
+            <Box
+              variants={box}
+              initial="invisible"
+              animate="visible"
+              exit="exit"
+              key={i}
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
+      <button onClick={prevPlease}>prev</button>
     </Wrapper>
   );
 }
