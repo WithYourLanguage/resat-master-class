51579e976acc1a5d0711db695f6d73516ca95e02
강 <ghang0501@gmail.com>
2023-06-11 20:42:30 +0900

    9.5 Home Screen part One
    강의 9.15에서는 Home Screen 중 데이터를 받아오는 것에 대해 진행하였다

---
+++ src/api.ts
@@ -0,0 +1,9 @@
+const API_KEY = "4f76628560fa503d406a872bacf54bd1";
+
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
